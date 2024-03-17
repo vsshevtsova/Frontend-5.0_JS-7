b0a15ed1c347d10ce0643a85cc0d97e7a3d34061
Valeria Shevtsova <vsshevtsova@yandex.ru>
2024-03-18 01:18:48 +0300

    js edit

--- Food/js/script.js
+++ Food/js/script.js
@@ -144,12 +144,13 @@ window.addEventListener("DOMContentLoaded", () => {
   // Классы для карточек
 
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector) {
+    constructor(src, alt, title, descr, price, parentSelector, ...classes) {
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
       this.transfer = 27;
       this.changeToUAH();
@@ -161,16 +162,21 @@ window.addEventListener("DOMContentLoaded", () => {
 
     render() {
       const element = document.createElement("div");
-      element.innerHTML = `<div class="menu__item">
-      <img src=${this.src} alt=${this.alt} />
-      <h3 class="menu__item-subtitle">${this.title}</h3>
-      <div class="menu__item-descr"> ${this.descr}
-      </div>
-      <div class="menu__item-divider"></div>
-      <div class="menu__item-price">
-        <div class="menu__item-cost">Цена:</div>
-        <div class="menu__item-total"><span>${this.price}</span> грн/день </div>
-      </div>
+      if (this.classes.length === 0) {
+        this.element = "menu__item";
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach((className) => element.classList.add(className));
+      }
+      element.innerHTML = `
+        <img src=${this.src} alt=${this.alt} />
+        <h3 class="menu__item-subtitle">${this.title}</h3>
+        <div class="menu__item-descr"> ${this.descr}
+        </div>
+        <div class="menu__item-divider"></div>
+        <div class="menu__item-price">
+          <div class="menu__item-cost">Цена:</div>
+          <div class="menu__item-total"><span>${this.price}</span> грн/день </div
     </div>`;
 
       this.parent.append(element);
@@ -188,8 +194,8 @@ window.addEventListener("DOMContentLoaded", () => {
   new MenuCard(
     "img/tabs/elite.jpg",
     "elite",
-    "Меню “Премиум”",
-    " В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!",
+    'Меню "Премиум"',
+    'В меню "Премиум" мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
     14,
     ".menu .container"
   ).render();
@@ -197,7 +203,7 @@ window.addEventListener("DOMContentLoaded", () => {
     "img/tabs/post.jpg",
     "post",
     'Меню "Постное"',
-    "Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.",
+    'Меню "Постное"- это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
     21,
     ".menu .container"
   ).render();
